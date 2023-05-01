dde1187f6931cf0441d5b861ce0e29f028536e18
Crazelu <eberelucky64@gmail.com>
2023-05-01 01:41:10 +0100

    chore: renames MaterialApp title and root app name, removes failing test (duh)

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,16 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await registerDependencies();
-  runApp(const MyApp());
+  runApp(const AudioBufferingDemoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AudioBufferingDemoApp extends StatelessWidget {
+  const AudioBufferingDemoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Audio Buffering Demo',
       theme: ThemeData(
         primarySwatch: Colors.pink,
         primaryColor: Colors.pink,
